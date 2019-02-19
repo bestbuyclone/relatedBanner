060137511c64b268db0c62a281762c105c7a1297
James Dempsey <james.dempsey420@gmail.com>
2019-02-19 16:47:56 -0600

    ready for import to proxy

---
+++ client/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>This is the Related Banner</div>;
+  }
+}
+
+export default App;
